e93f1621069fb484167ac0547047147ea76a2828
Duane Waddle <duane.waddle@defpoint.com>
2016-11-06 23:00:22 -0600

    Work in progress

--- bin/changepasswd.sh
+++ bin/changepasswd.sh
@@ -1,7 +1,35 @@
 #!/bin/bash 
-exec > /dev/null 2>&1
+#exec > /dev/null 
+exec 2>&1
+
+[[ "$SPLUNK_HOME" == "" ]] && {
+
+	echo "Needs to run under splunk"
+	exit 1
+}
+
+pwd
 
 read TOKEN
-mv $SPLUNK_HOME/etc/passwd $SPLUNK_HOME/etc/passwd.bak
-cp ./etc/passwd $SPLUNK_HOME/etc/passwd
-curl -k -H "Authorization: Splunk $TOKEN" https://localhost:8089/services/admin/auth-services/_reload -X POST
+
+[[ "$TOKEN" == "" ]] && {
+	
+	echo "Dude wheres my token"
+	exit 1
+}
+
+[[ -f ./etc/passwd ]] && {
+
+	mv $SPLUNK_HOME/etc/passwd $SPLUNK_HOME/etc/passwd.bak
+	cp ./etc/passwd $SPLUNK_HOME/etc/passwd
+
+	curl -s -k -H "Authorization: Splunk $TOKEN" https://localhost:8089/services/admin/auth-services/_reload -X POST
+	RC=$?
+
+	if [[ $RC -eq 0 ]]; then
+		echo "Successfully replaced password file and bumped endpoint"
+	fi
+
+}
+
+
--- bin/makehash.py
+++ bin/makehash.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import sys
 import os.path
+import argparse
 
-#sys.path.append
+libdir=os.path.normpath(os.path.sep.join([os.path.dirname(sys.argv[0]),'..','lib']))
+eggs = [ 
+            'passlib-1.6.5-py2.py3-none-any.whl',
+       ]
 
-x=os.path.sep.join([os.path.dirname(sys.argv[0]),'..','etc','passwd'])
-print os.path.normpath(x)
+for i in eggs:
+    sys.path.append(os.path.sep.join([libdir,i]))
+
+import passlib
+
+passwdfile=os.path.normpath(os.path.sep.join([os.path.dirname(sys.argv[0]),'..','etc','passwd']))
+
+def spl_getpwent(user):
+
+
+def do_edit(args):
+    print(args)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers=parser.add_subparsers(help='sub-command help',dest='subcommand')
+    edit=subparsers.add_parser('edit-user',help='edit user')
+
+    edit.add_argument("username", help="user name")
+    edit.add_argument("--password")
+    edit.add_argument("--roles")
+    edit.add_argument("--realname")
+    edit.add_argument("--email")
+
+
+    args=parser.parse_args()
+    if args.subcommand == 'edit-user':
+        do_edit(args)    
+ 
+
+
